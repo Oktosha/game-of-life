0b62ef810dec6f4881fe6eebf5f5b3199fe49c18
Daria Kolodzey <kolodaria@gmail.com>
2019-10-23 22:49:20 +0300

    Add reading state from file

--- game.py
+++ game.py
@@ -8,12 +8,30 @@ def printState(state, *, live="*", dead="."):
     print()
   print()
 
+
+def generateState(N=5, M=80):
+  return [[random.choice([True, False]) for j in range(M)] for i in range(N)]
+
+
+def readState(filename, *, live="*"):
+  f = open(filename)
+  raw_state = []
+  for line in f.readlines():
+    if (line.strip() != ""):
+      raw_state.append(line.strip())
+
+  N = len(raw_state)
+  M = len(raw_state[0])
+  return [[raw_state[i][j] == live for j in range(M)] for i in range(N)]
+
+
 def getCell(i, j, state):
   N = len(state)
   M = len(state[0])
   if i < 0 or j < 0 or i >= N or j >= M:
     return False
   return state[i][j]
+
 
 def countLiveNeighbours(i, j, state):
   ans = 0
@@ -23,20 +41,21 @@ def countLiveNeighbours(i, j, state):
         ans += 1 if getCell(i + di, j + dj, state) else 0
   return ans
 
+
 def nextCellState(i, j, state):
   if getCell(i, j, state):
     return (countLiveNeighbours(i, j, state) == 2
         or countLiveNeighbours(i, j, state) == 3)
   return  countLiveNeighbours(i, j, state) == 3
 
+
 def nextState(state):
   N = len(state)
   M = len(state[0])
   return [[nextCellState(i, j, state) for j in range(M)] for i in range(N)]
 
-N = 5
-M = 80
-state = [[random.choice([True, False]) for j in range(M)] for i in range(N)]
+
+state = readState("state.txt")
 
 while True:
   printState(state)
